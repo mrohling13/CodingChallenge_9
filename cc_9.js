6a7951c11d9819f714f87c613460f19cb48a9b13
mrohling13 <malachi.rohling@yahoo.com>
2025-02-22 19:42:53 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -18,3 +18,26 @@ class Employee {
 const emp1 = new Employee("Alice Johnson", "101", "Sales", 5000); // Employee Entry
 console.log(emp1.getDetails()); // Prints Employee Details
 
+// Task 2: Creating a Manager Class (Inheritance & Method Overriding)
+
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary); 
+        this.teamSize = teamSize; // Manager's team size
+    }; 
+    getDetails() {
+        return `Manager: ${this.name}, Id: ${this.id}, Department: ${this.department}, Salary: ${this.salary}, Team Size: ${this.teamSize}`;
+    } // manager details
+    calculateBonus() {
+        return this.salary * 12 * 0.10; // calculate bonus
+    }
+    // Task 4 modification
+    calculateAnnualSalary() {
+        return this.salary * 12 + this.calculateBonus();
+    }
+}; // manager class
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5); // new manager
+console.log(mgr1.getDetails()); // Print manager details
+console.log(mgr1.calculateBonus()); // Print manager bonus
+
